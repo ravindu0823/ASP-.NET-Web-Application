1e46f4ec1fe6a95ed34daabca4a324a0a6c6727e
shehanrandika99 <115799365+shehanrandika99@users.noreply.github.com>
2023-05-08 14:55:56 +0530

    created greencaptain login and home pages front-end (by nirodha)

--- Cleaning Management System/WebMasterGreenCaptain/Site.Master.designer.cs
+++ Cleaning Management System/WebMasterGreenCaptain/Site.Master.designer.cs
@@ -15,13 +15,13 @@ namespace WebMasterGreenCaptain
     {
 
         /// <summary>
-        /// HeadContent control.
+        /// color control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
+        protected global::System.Web.UI.HtmlControls.HtmlLink color;
 
         /// <summary>
         /// MainContent control.
